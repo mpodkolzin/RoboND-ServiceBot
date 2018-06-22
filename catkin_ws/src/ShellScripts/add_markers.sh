f65f875f36be26ad35f8fa488ee76d05a1de8d03
Max Podkolzin <mpodkolzin87@gmail.com>
2018-06-21 21:03:59 -0700

    (edit) Modified pathes in shell scripts

--- catkin_ws/src/ShellScripts/add_markers.sh
+++ catkin_ws/src/ShellScripts/add_markers.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/nvidia/catkin_ws/src/World/MyWorld.world" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/RoboND-ServiceBot/catkin_ws/src/World/MyWorld.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/nvidia/catkin_ws/src/World/MyWorld.yaml 3d_sensor:=kinect" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/RoboND-ServiceBot/catkin_ws/src/World/MyWorld.yaml 3d_sensor:=kinect" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
