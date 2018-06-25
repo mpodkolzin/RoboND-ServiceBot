8697ab8e007a0f7c3c5a0ab0f69b1348bd83ec46
Max Podkolzin <mpodkolzin87@gmail.com>
2018-06-25 15:31:49 -0700

    (update) Updated rviz launch

--- catkin_ws/src/ShellScripts/add_markers.sh
+++ catkin_ws/src/ShellScripts/add_markers.sh
@@ -3,7 +3,10 @@
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/RoboND-ServiceBot/catkin_ws/src/World/MyWorld.world" &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/RoboND-ServiceBot/catkin_ws/src/World/MyWorld.yaml 3d_sensor:=kinect" &
+
 sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+#xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "roslaunch add_markers view_navigation.launch" &
+
 sleep 5
 xterm -e " rosrun add_markers add_markers_node"
--- catkin_ws/src/ShellScripts/home_service.sh
+++ catkin_ws/src/ShellScripts/home_service.sh
@@ -4,7 +4,8 @@ xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/w
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=//home/workspace/RoboND-ServiceBot/catkin_ws/src/World/MyWorld.yaml 3d_sensor:=kinect" &
 sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+#xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "roslaunch add_markers view_navigation.launch" &
 sleep 5
 xterm -e " rosrun add_markers add_markers_node" &
 sleep 5
