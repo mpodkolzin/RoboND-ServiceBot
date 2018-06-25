681a60173f7ac9cd5be5031c8ea09f114a9c77b1
root <mpodkolzin87@gmail.com>
2018-06-25 23:53:37 +0000

    (add) modified according to review comments

---
+++ catkin_ws/build/worlds/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/RoboND-ServiceBot/catkin_ws/src/worlds
+# Build directory: /home/workspace/RoboND-ServiceBot/catkin_ws/build/worlds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
